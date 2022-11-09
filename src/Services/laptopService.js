2709b5ed2e5dc89f0e500c7b639629a20874a078
Eder Ramos <eder.ramosh@gmail.com>
2022-11-08 20:46:31 -0600

    url fixed

--- src/Services/laptopService.js
+++ src/Services/laptopService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const url = `${process.env.REACT_APP_API_KEY}/v1/`;
+const url = `${process.env.REACT_APP_API_KEY}/v1/laptop`;
 
 export const getLaptop = async () => {
     try {
-        const { data: { details } } = await axios.get(`${url}laptop/getLaptop`);
+        const { data: { details } } = await axios.get(`${url}/getLaptop`);
         // console.log(details);
         return details;
     } catch (e) {
@@ -15,7 +15,7 @@ export const getLaptop = async () => {
 
 export const getLaptopById = async ( id ) => {
     try {
-        const { data: { details } } = await axios.get(`${url}laptop/getLaptopById/${id}`);
+        const { data: { details } } = await axios.get(`${url}/getLaptopById/${id}`);
         // console.log( details );
         return details;
     } catch (e) {
