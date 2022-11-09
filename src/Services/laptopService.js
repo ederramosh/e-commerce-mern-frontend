7db3a553bbbc27bec4bfa3539434c9dacf7a87fb
Eder Ramos <eder.ramosh@gmail.com>
2022-11-08 20:53:03 -0600

    testing url

--- src/Services/laptopService.js
+++ src/Services/laptopService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const url = `${process.env.REACT_APP_API_KEY}/v1/laptop`;
+// const url = `${process.env.REACT_APP_API_KEY}/v1/laptop`;
 
 export const getLaptop = async () => {
     try {
-        const { data: { details } } = await axios.get(`${url}/getLaptop`);
+        const { data: { details } } = await axios.get(`${process.env.REACT_APP_API_KEY}/v1/laptop/getLaptop`);
         // console.log(details);
         return details;
     } catch (e) {
@@ -15,7 +15,7 @@ export const getLaptop = async () => {
 
 export const getLaptopById = async ( id ) => {
     try {
-        const { data: { details } } = await axios.get(`${url}/getLaptopById/${id}`);
+        const { data: { details } } = await axios.get(`${process.env.REACT_APP_API_KEY}/v1/laptop/getLaptopById/${id}`);
         // console.log( details );
         return details;
     } catch (e) {
